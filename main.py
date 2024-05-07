a33abac79195e61bbcd928738ccf70842b3cc747
Santiigas <santiagosof@gmail.com>
2024-05-06 23:53:16 -0300

    Janela com Tkinter

---
+++ main.py
@@ -0,0 +1,60 @@
+from tkinter import *
+from tkinter import ttk
+import string
+
+lista_comorbidades = ['Comorbidade1', 'Comorbidade2', 'Comorbidade3']
+lista_tempos = ['1:00','1:30','2:30', '4:00', '5:00']
+lista_dificuldade = ['Facil', 'Medio', 'Dificil']
+lista_forca = ['100', '200', '300']
+portas_arduino = ['COM1','COM2', 'COM3','COM4','COM5','COM6']
+
+
+#criação da janela
+janela = Tk()
+janela.title("Jogo do peixinho com arduino")
+janela.geometry("1040x700+155+70")
+janela.wm_resizable(width=False, height=False)
+
+#importação da imagem de fundo
+fundo_imagem = PhotoImage(file="imagens/template.png")
+
+#Label
+labelfundo = Label(janela, image=fundo_imagem)
+labelfundo.place(x=0, y=0)
+
+#Entradas -- Paciente
+entrada_nome_paciente = ttk.Entry(janela, justify=LEFT)
+entrada_nome_paciente.place(width=228, height=25, x=145, y=154)
+
+entrada_idade_paciente = ttk.Entry(janela, justify=LEFT)
+entrada_idade_paciente.place(width=228, height=25, x=145, y=190)
+
+entrada_comorbidade_paciente = ttk.Combobox(janela, justify=LEFT, values=lista_comorbidades)
+entrada_comorbidade_paciente.place(width=132, height=25, x=240, y=230)
+
+#Entradas -- Jogo
+
+entrada_tempo_do_jogo = ttk.Combobox(janela, justify=LEFT, values=lista_tempos)
+entrada_tempo_do_jogo .place(width=180, height=25, x=155, y=327)
+
+entrada_dificuldade_do_jogo = ttk.Combobox(janela, justify=LEFT, values=lista_dificuldade)
+entrada_dificuldade_do_jogo .place(width=122, height=25, x=212, y=369)
+
+entrada_forca_do_jogo = ttk.Combobox(janela, justify=LEFT, values=lista_forca)
+entrada_forca_do_jogo.place(width=195, height=25, x=138, y=407)
+
+#Entradas -- Jogo
+
+porta_arduino = ttk.Combobox(janela, justify=LEFT, values=portas_arduino)
+porta_arduino.place(width=175, height=25, x=138, y=519)
+
+frequencia_arduino = ttk.Combobox(janela, justify=LEFT)
+frequencia_arduino.place(width=103, height=25, x=210, y=562)
+
+#Botão
+botao1 = Button(janela, text="Jogar", relief='raised', command=lambda:PegarDadosDisciplina(1))
+botao1.place(width=100, height=33, x=900, y=562)
+
+
+
+janela.mainloop()
