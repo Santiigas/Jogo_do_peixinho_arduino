40200256cd85ee63e9de46d967a898d3403a9926
Santiigas <santiagosof@gmail.com>
2024-05-06 21:09:58 -0300

    +

--- painel_de_controle.py
+++ painel_de_controle.py
@@ -2,22 +2,32 @@ from random import randint
 import serial
 
 arduino = serial.Serial('COM6', 9600)
+
 while True:
-    linha = str(arduino.readline())
-    print(linha[2:-5])
+    linha = arduino.readline().decode().strip()
+    valor = int(linha) 
+    pulo = False
+    
+    if valor > 200:  
+        print(valor)
+        pulo = True
+
+
 arduino.close()
- 
+
+
+
 #informacões que vão vim do eletroneuromiografo
 gravidade = randint(5, 15)
 velocidade_do_salto = randint(20, 50)
 altura_maxima = randint(500, 550)
-pular = True
 #possiveis esparmos 
 
-def painel_de_controle(gravidade, velocidade_do_salto, altura_maxima, pular):
-    #Variaveis fixas (definidas no inicio)
-    velocidade_game = 10
-    tempo_game = 10
+def painel_de_controle():
 
-    return gravidade, velocidade_do_salto, altura_maxima, pular
+    print(gravidade,velocidade_do_salto, altura_maxima)
 
+painel_de_controle()
+
+teste = 123
+print(teste)
--- teste2.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-arduino = serial.Serial('COM3', 9600)
-while True:
-    linha = str(arduino.readline())
-    print(linha[2:-5])
-arduino.close()
-
-#tira a serial e instala a pyserial
